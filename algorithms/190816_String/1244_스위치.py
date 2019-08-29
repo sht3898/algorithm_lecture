244375da5794ddd06b370141bfa9190322d4e8ec
sht3898 <sehyunteg@naver.com>
2019-08-29 18:01:39 +0900

    190829 정리

--- algorithms/190816_String/1244_스위치.py
+++ algorithms/190816_String/1244_스위치.py
@@ -9,24 +9,25 @@ for _ in range(1, student+1):
     gender, number = map(int, input().split())
     number = number
     if gender == 1:
-        for i in range(1, number+1):
+        for i in range(1, N+1):
             if 0 <= number*i <= N:
                 if switch[number*i] == 0:
                     switch[number*i] = 1
                 else:
                     switch[number*i] = 0
-    elif gender == 2:
-        for i in range(number+1):
-            if switch[number+i] == switch[number-i]:
-                if switch[number+i] == 1:
-                    switch[number+i] = 0
-                    switch[number-i] = 0
-                elif switch[number+i] == 0:
-                    switch[number+i] = 1
-                    switch[number-i] = 1
+    else:
+        for j in range(N):
+            if 0 <= number + j <= N and 0 <= number - j <= N:
+                if switch[number+j] == switch[number-j]:
+                    if switch[number+j] == 0:
+                        switch[number+j] = 1
+                        switch[number+j] = 1
+                    else:
+                        switch[number+j] = 0
+                        switch[number+j] = 0
             else:
                 break
 
+
 switch = switch[1:]
-switch = map(str, switch)
-print(' '.join(switch))
+print(*switch)
